9dc6ef595115d025279a934872ba43ea7cc8f12f
Ajil Jalal <ajiljalal@gmail.com>
2019-06-22 13:42:53 -0500

    extract to folder

--- defense-gan-break/setup.sh
+++ defense-gan-break/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+tar -xvf break/intermediates/intermediates.tar -C ./break/intermediates/
 python3 -m venv --system-site-packages env
 source env/bin/activate
 python -m pip install --upgrade pip
