5b76223dce17596a7aa0145f88b9354f8cc69965
Ajil Jalal <ajiljalal@gmail.com>
2019-06-25 22:58:45 -0500

    modified eval.py to work with multiple robustness norms

--- defense-gan-break/break/eval.py
+++ defense-gan-break/break/eval.py
@@ -9,18 +9,21 @@ from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 #from models import Classifier
 
+#ch.manual_seed(0)#11241990)
 ch.set_default_tensor_type('torch.cuda.FloatTensor')
 
-BIG_BATCH_SIZE = 100
+BIG_BATCH_SIZE = 10#100
 BATCH_SIZE = 1
-R = 20
-L = 50000
+R = 10
+L = 200
 I = int(L*0.8)
 LAM = 0.00025
 CRIT = nn.CrossEntropyLoss(reduce=False)
 LARGE_NUM = 10000
 RANGE = ch.range(0,BIG_BATCH_SIZE*R-1).long()
-SAMPLES_PER_ITER=100
+SGD_LR = 10.
+folder_list = ['./intermediates_2.5','./intermediates_3.0','./intermediates_3.5', 'intermediates_3.0_200', './intermediates_3.0_300']
+NUM_FOLDERS = len(folder_list)
 
 gan = nn.DataParallel(ch.load("ckpts/netG")).cuda()
 for p in gan.parameters():
@@ -36,17 +39,17 @@ testloader = data.DataLoader(
 
 def latent_space_opt(ims, labels, num_steps=L):
     ims = ims.view(-1, 784)
-    zhat = ch.randn(R*BIG_BATCH_SIZE, 128)
+    zhat = ch.randn(R*ims.shape[0], 128)
     targets = ims.repeat(R, 1)
     zhat.requires_grad_()
-    opt = optim.SGD([zhat], lr=500.0, momentum=0.7)
+    opt = optim.SGD([zhat], lr=SGD_LR, momentum=0.7)
     lr_maker = StepLR(opt, step_size=I)
 
     for i in range(num_steps):
         opt.zero_grad()
         loss_mat = ((gan(zhat) - targets)**2).mean(-1)
         total_loss = loss_mat.clone()
-        if i % 500 == 0:
+        if (i+1) % 100 == 0:
             print("Iteration %d | Distance Loss %f" % (i, loss_mat.mean()))
         total_loss.mean().backward()
         opt.step()
@@ -56,7 +59,7 @@ def latent_space_opt(ims, labels, num_steps=L):
     image_mat = ch.stack(gan(zhat).chunk(R, 0), 0)
     zh_mat = ch.stack(zhat.chunk(R, 0), 0)
     ind = (-distance_mat).argmax(0)
-    im_range = ch.range(0,BIG_BATCH_SIZE-1).long()
+    im_range = ch.range(0,ims.shape[0]-1).long()
     best_ims = image_mat[ind,im_range,:]
     best_zhs = zh_mat[ind,im_range,:]
 
@@ -64,7 +67,7 @@ def latent_space_opt(ims, labels, num_steps=L):
 
 def accuracy(): 
     total_correct = 0.0
-    total_correct_inbds = 0.0
+    total_incorrect_inbds = 0.0
     total = 0.0
     i = 0
     try:
@@ -73,23 +76,40 @@ def accuracy():
             all_ints = []
             all_orig = []
             for j in range(BIG_BATCH_SIZE//BATCH_SIZE):
-                intermediates = ch.load("intermediates/batch_%d_attack" % (i,))
-                originals = ch.load("intermediates/batch_%d_orig" % (i,))
-                all_ints.append(intermediates)
-                all_orig.append(originals)
+                for folder in folder_list: 
+                    intermediates = ch.load("%s/batch_%d_attack" % (folder,i,))
+                    originals = ch.load("%s/batch_%d_orig" % (folder,i,))
+                    all_ints.append(intermediates)
+                    all_orig.append(originals)
                 i += 1
             intermediates = ch.cat(all_ints, 0)
             originals = ch.cat(all_orig, 0)
-            images, _ = latent_space_opt(intermediates.view(-1, 784).cuda(), targets, num_steps=20000)
+            images, _ = latent_space_opt(intermediates.view(-1, 784).cuda(), targets, num_steps=L)
             with ch.no_grad():
                 out = cla(images.view(-1,1,28,28).cuda())
+                
                 preds = out.argmax(1)
-                total_correct += (preds == targets).float().sum()
-                norms = (intermediates-originals.view(BIG_BATCH_SIZE, -1)).float().pow(2).sum(-1).pow(0.5)
-                total_correct_inbds += ((1 - (preds == targets).float())*((norms < 4).float())).sum()
-                total += preds.shape[0]
-    except:
-        return total_correct/total, total_correct_inbds/total
+                preds_repeat = ch.stack(preds.chunk(BIG_BATCH_SIZE),0)
+                
+                targets_repeat = ch.stack(targets.repeat(NUM_FOLDERS).chunk(NUM_FOLDERS),1)
+                print(preds_repeat, targets_repeat)
+                correct = preds_repeat == targets_repeat
 
-a,b = accuracy()
-print("Adversarial accuracy %f | Adversarial in-bounds accuracy %f" % (a.item(), 1-b.item()))
+                total_correct += ch.all(correct,1).float().sum()
+                total += preds_repeat.shape[0]
+
+                norms = (intermediates-originals.view(originals.shape[0], -1)).float().pow(2).sum(-1).pow(0.5)
+                norms = ch.stack(norms.chunk(BIG_BATCH_SIZE),0)
+                print(norms)
+                print(norms.shape)
+                total_incorrect_inbds += ch.any((1-correct)*(norms<4),1).float().sum()
+                #total_correct += (preds == targets).float().sum()
+                #norms = (intermediates-originals.view(BIG_BATCH_SIZE, -1)).float().pow(2).sum(-1).pow(0.5)
+                #total_incorrect_inbds += ((1 - (preds == targets).float())*((norms < 4).float())).sum()
+                #total += preds.shape[0]
+            print(total_correct/total, total_incorrect_inbds/total)
+    except:
+        return total, total_correct/total, total_incorrect_inbds/total
+
+total, a,b = accuracy()
+print("Total %f | Adversarial accuracy %f | Adversarial in-bounds accuracy %f" % (total,a.item(), 1-b.item()))
