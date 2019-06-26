316c66f20a27bfd561d835e2a8b80996e3f32cd7
Ajil Jalal <ajiljalal@gmail.com>
2019-06-25 23:05:00 -0500

    modified setup script

--- defense-gan-break/setup.sh
+++ defense-gan-break/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-tar -xvf break/intermediates/intermediates.tar -C ./break/intermediates/
+tar -xvf break/intermediates_1.0/intermediates.tar -C ./break/intermediates_1.0/
+tar -xvf break/intermediates_2.5/intermediates_2.5.tar -C ./break/intermediates_2.5
+tar -xvf break/intermediates_3.0/intermediates_3.0.tar -C break/intermediates_3.0/
+tar -xvf break/intermediates_3.5/intermediates_3.5.tar -C break/intermediates_3.5/
+tar -xvf break/intermediates_3.0_200/intermediates_3.0_200.tar -C break/intermediates_3.0_200/
+tar -xvf break/intermediates_3.0_300/intermediates_3.0_300.tar -C break/intermediates_3.0_300/
 python3 -m venv --system-site-packages env
 source env/bin/activate
 python -m pip install --upgrade pip
